9dd75cbf882239e99170c9a4bf472692b7847f06
proauto <hg1771@naver.com>
2022-10-17 23:31:16 +0900

    second push

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ app.use(bodyParser.urlencoded({extended : true}));
 const MongoClient = require('mongodb').MongoClient
 app.set('view engine', 'ejs');
 
+app.use('/public', express.static('public'));
+const methodOverride = require('method-override')
+app.use(methodOverride('_method')) 
+
 var db;
 MongoClient.connect('mongodb+srv://admin:qwer1234@cluster0.dwqfqrz.mongodb.net/?retryWrites=true&w=majority',function(에러, client){
    
@@ -33,11 +37,11 @@ app.get('/beauty',(요청, 응답) => {
 });
 
 app.get('/',(요청, 응답) => {
-    응답.sendFile(__dirname + '/index.html');
+    응답.render('index.ejs',);
 });
 
 app.get('/write', (요청, 응답) => { 
-    응답.sendFile(__dirname +'/write.html')
+    응답.render('write.ejs',);
   });
 
 //어떤 사람이 add 경로로 POST 요청을 하면 데이터 2개(날짜, 제목을 보내주는데
@@ -81,3 +85,25 @@ app.delete('/delete',function(요청, 응답){
         응답.status(200).send({ message : '성공했습니다'});
     })
 })
+
+app.get('/detail/:id', function(요청, 응답){
+    db.collection('post').findOne({ _id : parseInt(요청.params.id) }, function(에러, 결과){
+      응답.render('detail.ejs', {data : 결과} )
+    })
+  });
+
+app.get('/edit/:id',function(요청,응답){
+    db.collection('post').findOne({ _id : parseInt(요청.params.id) }, function(에러, 결과){
+    응답.render('edit.ejs', {post : 결과} )
+    })
+});
+
+app.put('/edit', function(요청, 응답){
+    //폼에 담긴 제목 날짜 데이터를 가지고 db를 업데이트함
+    
+    db.collection('post').updateOne({ _id : parseInt(요청.body.id)},{$set : {제목 : 요청.body.title, 날짜 : 요청.body.date}},function(에러, 결과){
+        console.log('수정완료')
+        응답.redirect('/list')
+    });
+
+});
